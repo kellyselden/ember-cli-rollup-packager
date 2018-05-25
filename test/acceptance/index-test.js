0df7e216b0d74ce78e036878c76a2af21ac6d6fa
Kelly Selden <kellyselden@gmail.com>
2018-05-24 19:56:55 -0700

    add basic test

--- test/acceptance/index-test.js
+++ test/acceptance/index-test.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const { expect } = require('chai');
+const spawn = require('cross-spawn');
 
 describe('Acceptance | index', function() {
+  this.timeout(60000);
+
   it('works', function() {
+    process.chdir('test/fixtures/my-app');
+    spawn.sync('node', ['node_modules/ember-cli/bin/ember', 'test'], { stdio: 'inherit' });
     expect(true).to.be.ok;
   });
 });
