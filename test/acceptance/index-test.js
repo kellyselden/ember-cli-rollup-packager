c786e984c5f39b3337e3cc9bdf2a6eee45b235c4
Kelly Selden <kellyselden@gmail.com>
2018-05-24 20:08:10 -0700

    use spawnSync

--- test/acceptance/index-test.js
+++ test/acceptance/index-test.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const { expect } = require('chai');
-const spawn = require('cross-spawn');
+const { spawnSync } = require('child_process');
 
 describe('Acceptance | index', function() {
   this.timeout(60000);
 
   it('works', function() {
     process.chdir('test/fixtures/my-app');
-    spawn.sync('node', ['node_modules/ember-cli/bin/ember', 'test'], { stdio: 'inherit' });
+    spawnSync('node', ['node_modules/ember-cli/bin/ember', 'test'], { stdio: 'inherit' });
     expect(true).to.be.ok;
   });
 });
