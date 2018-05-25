dac6c556af98141de6ae6aa5adbc0fd5d0be939f
Kelly Selden <kellyselden@gmail.com>
2018-05-24 20:34:41 -0700

    update test

--- test/acceptance/index-test.js
+++ test/acceptance/index-test.js
@@ -2,13 +2,20 @@
 
 const { expect } = require('chai');
 const { spawnSync } = require('child_process');
+const resolve = require('resolve');
+const path = require('path');
 
 describe('Acceptance | index', function() {
   this.timeout(60000);
 
   it('works', function() {
     process.chdir('test/fixtures/my-app');
-    spawnSync('node', ['node_modules/ember-cli/bin/ember', 'test'], { stdio: 'inherit' });
-    expect(true).to.be.ok;
+
+    let entryPoint = resolve.sync('ember-cli', { basedir: process.cwd() });
+    let bin = path.resolve(entryPoint, '../../../bin/ember');
+
+    let { status } = spawnSync('node', [bin, 'test'], { stdio: 'inherit' });
+
+    expect(status).to.equal(0);
   });
 });
