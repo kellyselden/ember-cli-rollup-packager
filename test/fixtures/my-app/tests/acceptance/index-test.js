24e163a7e95ba86d12470d72da7531bb3cacec47
Kelly Selden <kellyselden@gmail.com>
2018-07-16 09:18:08 +0100

    handle new tests location

--- test/fixtures/my-app/tests/acceptance/index-test.js
+++ test/fixtures/my-app/tests/acceptance/index-test.js
@@ -1,6 +1,11 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import foo from 'my-app/imported-from-tests';
+import bar from 'my-app/tests/helpers/imported-from-tests';
+
+foo();
+bar();
 
 module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
